0ad8d8c80437bf4685b19a517bc32bd5758e78d6
QanT Setsuna F. Seiei ✅ <AndrewPham1998@yahoo.com>
2023-07-05 09:15:29 -0700

    js edit

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function newInventory(){
 
 newInventory() 
 newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
+newImage('assets/tree.png', 150, 450)
+newImage('assets/pillar.png', 300, 250)
+newImage('assets/pine-tree.png', 400, 350)
+newImage('assets/crate.png', 125, 350)
 newImage('assets/well.png', 500, 575)
 
-newItem('assets/sword.png', 500, 555)
+newItem('assets/sword.png', 450, 550)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
 
